d21fe3f30bad5395de042db6d52d66b2e4df60bb
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-06-26 20:47:30 +0900

    added account 1 info

--- script.js
+++ script.js
@@ -59,6 +59,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//MOVEMENTS
+
+const displayMovements =  function (movements) {
+  containerMovements.innerHTML = ''
+  movements.forEach(function(mov, i){
+    const type = mov > 0 ? `deposit`: `withdrawal`
+    const html = `
+    <div class="movements">
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i +1 }  ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -72,3 +93,11 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const euroToUSD = 1.1;
+
+// const movementUSD = movements.map(function (mov){
+//   return mov * eurToUSD;
+// });
+
+const movementsUSD = movements.map(mov => mov * euroUSD);
